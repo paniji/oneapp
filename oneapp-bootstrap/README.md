1fcb31176695c4df7d1a3cd338dd525ef1222eb1
AWS CodeBuild <pani-aws-codebuild@ap.com>
2023-04-13 10:35:05 -0400

    rm bt

--- oneapp-bootstrap/README.md
+++
@@ -1,1 +0,0 @@
-# oneapp-bootstrap
--- oneapp-bootstrap/cdk-bootstrap/files/zip_repo.sh
+++
@@ -1,6 +0,0 @@
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-echo $SCRIPT_DIR
-#mkdir repository-code
-pushd $SCRIPT_DIR
-zip -qr ./repository-code.zip .
-popd
